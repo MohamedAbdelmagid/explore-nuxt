0b8807688d206bc0abdb31228a89a31d95fb4288
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2020-07-21 16:58:05 -0700

    Deployment

---
+++ pages/index.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        explore-nuxt
+      </h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <nuxt-link
+          v-for="post in posts"
+          :to="{ name: 'posts-id', params: { id: post.id } }"
+          :key="post.id"
+          class="button--grey"
+        >
+          {{ post.title }}
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    head () {
+      return {
+        title: 'Home Page 🍕',
+        meta: [
+          { name: 'twitter:title', content: 'Nuxt Fundamentals'},
+          { name: 'twitter:description', content: 'Exploring Nuxt by example = 🍕'},
+          { name: 'twitter:image', content: 'https://i.imgur.com/UYP2umJ.png'},
+          { name: 'twitter:card', content: 'summary_large_image'}
+        ]
+      }
+    },
+    computed: {
+      posts () {
+        return this.$store.state.post.posts
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ pages/posts/_id.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container">
+    <article>
+      <h1 class="title">{{ post.title }}</h1>
+      <p>{{ post.content }}</p>
+    </article>
+    <aside>
+      <h3>Posts you might like</h3>
+      <ul>
+        <li v-for="post in relatedPosts" :key="post.id">
+          <nuxt-link :to="{ name: 'posts-id', params: { id: post.id } }">{{ post.title }}</nuxt-link>
+        </li>
+      </ul>
+    </aside>
+  </div>
+</template>
+
+<script>
+  export default {
+    head () {
+      return {
+        title: this.post.title,
+        meta: [
+          { name: 'twitter:title', content: this.post.title},
+          { name: 'twitter:description', content: this.post.content},
+          { name: 'twitter:image', content: 'https://i.imgur.com/UYP2umJ.png'},
+          { name: 'twitter:card', content: 'summary_large_image'}
+        ]
+      }
+    },
+    
+    data() {
+      return {
+        id: this.$route.params.id,
+
+      }
+    },
+
+    computed: {
+      post () {
+        return this.$store.state.post.posts.find(post => post.id === this.id)
+      },
+      relatedPosts () {
+        return this.$store.state.post.posts.filter(post => post.id !== this.id)
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  .container {
+    display: flex;
+    justify-content: space-between;
+    line-height: 1.5;
+  }
+  article * {
+    margin-bottom: 1rem;
+  }
+  aside {
+    min-width: 280px;
+    max-width: 280px;
+    padding-left: 2rem;
+  }
+  .title {
+    font-size: 2rem;
+  }
+</style>
